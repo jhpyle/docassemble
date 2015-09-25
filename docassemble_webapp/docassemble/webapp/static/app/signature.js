a7b929933ae14eec15aa3364c1b1260e8f8b20d8
Jonathan Pyle <jpyle@philalegal.org>
2015-09-25 14:02:29 -0400

    prevent too many clicks on Done button

--- docassemble_webapp/docassemble/webapp/static/app/signature.js
+++ docassemble_webapp/docassemble/webapp/static/app/signature.js
@@ -29,6 +29,7 @@ $(document).ready(function () {
     newCanvas();
   });
   $("#save").click(function() {
+    document.getElementById('save').disabled = true;
     saveCanvas();
   });
   window.scrollTo(0,1);
