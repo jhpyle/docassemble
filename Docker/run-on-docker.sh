355f7ee348469c26a9f19eb3bad0e703836ae30e
Jonathan Pyle <jpyle@philalegal.org>
2015-11-29 23:06:31 -0500

    supervisord adjustments

--- Docker/run-on-docker.sh
+++ Docker/run-on-docker.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 /etc/init.d/postgresql start
 /etc/init.d/apache2 start
-exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
+/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
+/etc/init.d/apache2 stop
+/etc/init.d/postgresql stop
