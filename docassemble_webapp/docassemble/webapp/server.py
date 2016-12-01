8be130c6c7ba762b4356be2e20c3acadde1f323b
Jonathan Pyle <jpyle@philalegal.org>
2016-12-01 01:35:37 -0500

    attempt

--- docassemble_webapp/docassemble/webapp/server.py
+++ docassemble_webapp/docassemble/webapp/server.py
@@ -670,7 +670,7 @@ def setup_app():
     from docassemble.webapp.users.models import UserModel, UserAuthModel, MyUserInvitation
     from flask_login import LoginManager
     from flask_user import UserManager, SQLAlchemyAdapter
-    db_adapter = SQLAlchemyAdapter(db, UserModel, UserAuthClass=UserAuthModel)
+    db_adapter = SQLAlchemyAdapter(db, UserModel, UserAuthClass=UserAuthModel, UserInvitationClass=MyUserInvitation)
     #db_adapter.UserInvitationClass = MyUserInvitation
     user_manager = UserManager(db_adapter, app, register_form=MyRegisterForm, user_profile_view_function=user_profile_page, logout_view_function=logout, login_view_function=custom_login, unauthorized_view_function=unauthorized, unauthenticated_view_function=unauthenticated)
     lm = LoginManager(app)
