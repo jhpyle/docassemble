0b3ee3587854233ea6fa81f04a0089257dc919ae
Jonathan Pyle <jhpyle@gmail.com>
2017-06-02 20:46:10 -0400

    bug fixes

--- docassemble_webapp/docassemble/webapp/restart.py
+++ docassemble_webapp/docassemble/webapp/restart.py
@@ -1,21 +1,21 @@
 import sys
 import os
 import docassemble.base.config
-from docassemble.base.config import daconfig, da_config_file
 
 if __name__ == "__main__":
     docassemble.base.config.load(arguments=sys.argv)
-WEBAPP_PATH = daconfig.get('webapp', '/usr/share/docassemble/webapp/docassemble.wsgi')
 
 def main():
+    from docassemble.base.config import daconfig
+    webapp_path = daconfig.get('webapp', '/usr/share/docassemble/webapp/docassemble.wsgi')
     import docassemble.webapp.cloud
     cloud = docassemble.webapp.cloud.get_cloud()
     if cloud is not None:
         key = cloud.get_key('config.yml')
         if key.exists():
-            key.get_contents_to_filename(da_config_file)
+            key.get_contents_to_filename(daconfig['config file'])
             sys.stderr.write("Wrote config file based on copy on cloud\n")
-    wsgi_file = WEBAPP_PATH
+    wsgi_file = webapp_path
     if os.path.isfile(wsgi_file):
         with open(wsgi_file, 'a'):
             os.utime(wsgi_file, None)
