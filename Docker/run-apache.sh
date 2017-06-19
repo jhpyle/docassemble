8a176254d790e60f3865520970c60ffcb76352f5
Jonathan Pyle <jhpyle@gmail.com>
2017-06-18 22:54:21 -0400

    remove sync autostart

--- Docker/run-apache.sh
+++ Docker/run-apache.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export DA_ACTIVATE="${DA_PYTHON:-/usr/share/docassemble/local}/bin/activate"
-export DA_CONFIG_FILE="${DA_CONFIG:-/usr/share/docassemble/config/config.yml}"
-source /dev/stdin < <(su -c "source $DA_ACTIVATE && python -m docassemble.base.read_config $DA_CONFIG_FILE" www-data)
-
 function stopfunc {
     /usr/sbin/apache2ctl stop
     exit 0
