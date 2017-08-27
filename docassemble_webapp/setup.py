2233f5bf98464031ca6b48d383b50a10c1998e23
Jonathan Pyle <jhpyle@gmail.com>
2017-08-27 12:12:24 -0400

    Bump version: 0.1.42 → 0.1.43

--- docassemble_webapp/setup.py
+++ docassemble_webapp/setup.py
@@ -3,7 +3,7 @@
 import os
 from setuptools import setup, find_packages
 setup(name='docassemble.webapp',
-      version='0.1.42',
+      version='0.1.43',
       description=('The web application components of the docassemble system.'),
       author='Jonathan Pyle',
       author_email='jhpyle@gmail.com',
