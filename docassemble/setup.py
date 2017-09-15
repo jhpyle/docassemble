dd2d54f4dabed9cb4270dab2e9df3a2692e89e65
Jonathan Pyle <jhpyle@gmail.com>
2017-09-15 08:33:24 -0400

    Bump version: 0.1.51 → 0.1.52

--- docassemble/setup.py
+++ docassemble/setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='docassemble',
-      version='0.1.51',
+      version='0.1.52',
       description=('A system for assembling documents from templates while automatically querying a user for necessary information.'),
       author='Jonathan Pyle',
       author_email='jhpyle@gmail.com',
