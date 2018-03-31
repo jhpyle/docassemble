2803e8657a33a6fbb9867ea5ace4c5d3ae9ce58a
Jonathan Pyle <jhpyle@gmail.com>
2018-03-31 16:59:38 -0400

    google analytics fix

--- docassemble_webapp/docassemble/webapp/server.py
+++ docassemble_webapp/docassemble/webapp/server.py
@@ -5325,8 +5325,8 @@ def index():
         question_id = interview_status.question.id
     else:
         question_id = None;
-    interview_filename = re.sub(r':.*', '', yaml_filename)
-    interview_package = re.sub(r'.*\/', '', yaml_filename)
+    interview_package = re.sub(r':.*', '', yaml_filename)
+    interview_filename = re.sub(r'.*[:\/]', '', yaml_filename)
     if not is_ajax:
         scripts = standard_scripts()
         if 'google maps api key' in google_config:
