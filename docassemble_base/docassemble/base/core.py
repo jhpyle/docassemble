fd39d7792d90cd1b178f472a1772b17d89047084
Jonathan Pyle <jhpyle@gmail.com>
2018-10-16 10:19:56 -0400

    editing table will run complete_elements

--- docassemble_base/docassemble/base/core.py
+++ docassemble_base/docassemble/base/core.py
@@ -997,7 +997,14 @@ class DAList(DAObject):
                 use_edit = not val.get('edit', True)
                 use_delete = not val.get('delete', True)
         if use_edit:
-            output += '<a href="' + docassemble.base.functions.url_action('_da_list_edit', items=[item.instanceName + ('' if y.startswith('[') else '.') + y for y in the_args]) + '" class="btn btn-sm btn-secondary btn-revisit"><i class="fas fa-pencil-alt"></i> ' + word('Edit') + '</a> '
+            if self.complete_attribute is not None:
+                items = [dict(action='_da_undefine', arguments=dict(variables=[item.instanceName + '.' + self.complete_attribute]))]
+            else:
+                items = []
+            items += [item.instanceName + ('' if y.startswith('[') else '.') + y for y in the_args]
+            if self.complete_attribute is not None:
+                items += [dict(action='_da_compute', arguments=dict(variables=[item.instanceName + '.' + self.complete_attribute]))]
+            output += '<a href="' + docassemble.base.functions.url_action('_da_list_edit', items=items) + '" class="btn btn-sm btn-secondary btn-revisit"><i class="fas fa-pencil-alt"></i> ' + word('Edit') + '</a> '
         if use_delete and can_delete:
             output += '<a href="' + docassemble.base.functions.url_action('_da_list_remove', list=self.instanceName, item=repr(index)) + '" class="btn btn-sm btn-danger btn-revisit"><i class="fas fa-trash"></i> ' + word('Delete') + '</a>'
         return output
@@ -1660,7 +1667,14 @@ class DADict(DAObject):
                 use_edit = not val.get('edit', True)
                 use_delete = not val.get('delete', True)
         if use_edit:
-            output += '<a href="' + docassemble.base.functions.url_action('_da_dict_edit', items=[item.instanceName + ('' if y.startswith('[') else '.') + y for y in the_args]) + '" class="btn btn-sm btn-secondary btn-revisit"><i class="fas fa-pencil-alt"></i> ' + word('Edit') + '</a> '
+            if self.complete_attribute is not None:
+                items = [dict(action='_da_undefine', arguments=dict(variables=[item.instanceName + '.' + self.complete_attribute]))]
+            else:
+                items = []
+            items += [item.instanceName + ('' if y.startswith('[') else '.') + y for y in the_args]
+            if self.complete_attribute is not None:
+                items += [dict(action='_da_compute', arguments=dict(variables=[item.instanceName + '.' + self.complete_attribute]))]
+            output += '<a href="' + docassemble.base.functions.url_action('_da_dict_edit', items=items) + '" class="btn btn-sm btn-secondary btn-revisit"><i class="fas fa-pencil-alt"></i> ' + word('Edit') + '</a> '
         if use_delete:
             output += '<a href="' + docassemble.base.functions.url_action('_da_dict_remove', dict=self.instanceName, item=repr(index)) + '" class="btn btn-sm btn-danger btn-revisit"><i class="fas fa-trash"></i> ' + word('Delete') + '</a>'
         return output
