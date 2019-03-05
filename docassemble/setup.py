88e96e63b093a830307cfe9dcc68f88fe4cd1593
Jonathan Pyle <jhpyle@gmail.com>
2019-03-04 23:38:36 -0500

    Bump version: 0.4.15 → 0.4.16

--- docassemble/setup.py
+++ docassemble/setup.py
@@ -5,7 +5,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='docassemble',
-      version='0.4.15',
+      version='0.4.16',
       description=('The namespace package for the docassemble system.'),
       long_description=read("README.md"),
       long_description_content_type='text/markdown',
