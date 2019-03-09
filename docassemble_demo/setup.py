56fcc932f19c3c74d24ad0f70caae58e0588b68a
Jonathan Pyle <jhpyle@gmail.com>
2019-03-09 16:25:47 -0500

    Bump version: 0.4.17 → 0.4.18

--- docassemble_demo/setup.py
+++ docassemble_demo/setup.py
@@ -46,7 +46,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.demo',
-      version='0.4.17',
+      version='0.4.18',
       description=('A demonstration package for docassemble.'),
       long_description=read("README.md"),
       long_description_content_type='text/markdown',
@@ -57,7 +57,7 @@ setup(name='docassemble.demo',
       download_url='https://download.docassemble.org/docassemble-demo.tar.gz',
       packages=find_packages(),
       namespace_packages = ['docassemble'],
-      install_requires = ['docassemble==0.4.17', 'docassemble.base==0.4.17', 'python-dateutil'],
+      install_requires = ['docassemble==0.4.18', 'docassemble.base==0.4.18', 'python-dateutil'],
       zip_safe = False,
       package_data=find_package_data(where=os.path.join('docassemble', 'demo', ''), package='docassemble.demo'),
   )
