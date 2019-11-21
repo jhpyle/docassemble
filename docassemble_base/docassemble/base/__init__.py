e2514f3d5e52f9d36f2a64178785c1545c9a2053
Jonathan Pyle <jhpyle@gmail.com>
2019-11-21 09:18:08 -0500

    Bump version: 0.5.45 → 0.5.46

--- docassemble_base/docassemble/base/__init__.py
+++ docassemble_base/docassemble/base/__init__.py
@@ -2,4 +2,4 @@ try:
     __import__('pkg_resources').declare_namespace(__name__)
 except ImportError:
     __path__ = __import__('pkgutil').extend_path(__path__, __name__)
-__version__ = "0.5.45"
+__version__ = "0.5.46"
