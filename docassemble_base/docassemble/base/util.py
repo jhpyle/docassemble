b4cef906bb90574ff4f6b67b2a965cfb16f0e9f7
Jonathan Pyle <jhpyle@gmail.com>
2019-11-24 22:47:29 -0500

    Python 2.7 error

--- docassemble_base/docassemble/base/util.py
+++ docassemble_base/docassemble/base/util.py
@@ -2529,16 +2529,15 @@ def get_status(setting):
 
 def prevent_dependency_satisfaction(f):
     def wrapper(*args, **kwargs):
-        if PY2:
-            try:
-                return f(*args, **kwargs)
-            except Exception as err:
-                raise Exception("Reference to undefined variable in context where dependency satisfaction not allowed")
-        else:
-            try:
-                return f(*args, **kwargs)
-            except (NameError, AttributeError, DAIndexError, UndefinedError) as err:
-                raise Exception("Reference to undefined variable in context where dependency satisfaction not allowed") from err
+        try:
+            return f(*args, **kwargs)
+        except (NameError, AttributeError, DAIndexError, UndefinedError) as err:
+            raise Exception("Reference to undefined variable in context where dependency satisfaction not allowed")
+        # Python 3 version:
+        # try:
+        #     return f(*args, **kwargs)
+        # except (NameError, AttributeError, DAIndexError, UndefinedError) as err:
+        #     raise Exception("Reference to undefined variable in context where dependency satisfaction not allowed") from err
     return wrapper
 
 from docassemble.base.oauth import DAOAuth
