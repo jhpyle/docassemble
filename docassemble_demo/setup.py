d413c5a299f88b2570f74d2520aa54777e6538c7
Jonathan Pyle <jhpyle@gmail.com>
2020-02-29 18:31:21 -0500

    Bump version: 0.5.105 → 0.5.106

--- docassemble_demo/setup.py
+++ docassemble_demo/setup.py
@@ -46,7 +46,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.demo',
-      version='0.5.105',
+      version='0.5.106',
       python_requires='>=3.5',
       description=('A demonstration package for docassemble.'),
       long_description=read("README.md"),
@@ -58,7 +58,7 @@ setup(name='docassemble.demo',
       download_url='https://download.docassemble.org/docassemble-demo.tar.gz',
       packages=find_packages(),
       namespace_packages = ['docassemble'],
-      install_requires = ['docassemble==0.5.105', 'docassemble.base==0.5.105', "python-dateutil==2.8.0"],
+      install_requires = ['docassemble==0.5.106', 'docassemble.base==0.5.106', "python-dateutil==2.8.0"],
       zip_safe = False,
       package_data=find_package_data(where=os.path.join('docassemble', 'demo', ''), package='docassemble.demo'),
   )
