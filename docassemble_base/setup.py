2f77d49107d3d2b4c2e9cf1d768b1d1fb8bbd806
Jonathan Pyle <jhpyle@gmail.com>
2020-04-30 23:40:29 -0400

    Bump version: 1.1.21 → 1.1.22

--- docassemble_base/setup.py
+++ docassemble_base/setup.py
@@ -48,7 +48,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 openpyxl_version = "2.5.14" if sys.version.startswith('3.5') else "3.0.0"
 twine_version = "1.15.0" if sys.version.startswith('3.5') else "2.0.0"
 install_requires = [
-    'docassemble==1.1.21',
+    'docassemble==1.1.22',
     "3to2==1.1.1",
     "alembic==1.2.1",
     "astunparse==1.6.2",
@@ -175,7 +175,7 @@ install_requires = [
 ]
 
 setup(name='docassemble.base',
-      version='1.1.21',
+      version='1.1.22',
       python_requires='>=3.5',
       description=('The base components of the docassemble system.'),
       long_description=read("README.md"),
