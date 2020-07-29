dfbab96f150c1c100a9019b539baef189fd2a59b
Jonathan Pyle <jhpyle@gmail.com>
2020-07-29 10:13:39 -0400

    Bump version: 1.1.52 → 1.1.53

--- docassemble/setup.py
+++ docassemble/setup.py
@@ -5,7 +5,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='docassemble',
-      version='1.1.52',
+      version='1.1.53',
       python_requires='>=3.5',
       description=('The namespace package for the docassemble system.'),
       long_description=read("README.md"),
