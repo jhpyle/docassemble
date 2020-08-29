7b1e5d44b58099a5f2a1183cbbac76f2a542e4bb
Jonathan Pyle <jhpyle@gmail.com>
2020-08-28 22:53:29 -0400

    Bump version: 1.1.64 → 1.1.65

--- docassemble/docassemble/__init__.py
+++ docassemble/docassemble/__init__.py
@@ -2,4 +2,4 @@ try:
     __import__('pkg_resources').declare_namespace(__name__)
 except ImportError:
     __path__ = __import__('pkgutil').extend_path(__path__, __name__)
-__version__ = "1.1.64"
+__version__ = "1.1.65"
