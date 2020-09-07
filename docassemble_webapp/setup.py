754bcd943daf8b0a26ff1efe07c94f3ff26372ab
Jonathan Pyle <jhpyle@gmail.com>
2020-09-07 11:51:51 -0400

    Bump version: 1.1.70 → 1.1.71

--- docassemble_webapp/setup.py
+++ docassemble_webapp/setup.py
@@ -8,9 +8,9 @@ def read(fname):
 openpyxl_version = "2.5.14" if sys.version.startswith('3.5') else "3.0.0"
 twine_version = "1.15.0" if sys.version.startswith('3.5') else "2.0.0"
 install_requires = [
-    'docassemble==1.1.70',
-    'docassemble.base==1.1.70',
-    'docassemble.demo==1.1.70',
+    'docassemble==1.1.71',
+    'docassemble.base==1.1.71',
+    'docassemble.demo==1.1.71',
     "3to2==1.1.1",
     "airtable-python-wrapper==0.12.0",
     "alembic==1.2.1",
@@ -205,7 +205,7 @@ install_requires = [
 ]
 
 setup(name='docassemble.webapp',
-      version='1.1.70',
+      version='1.1.71',
       python_requires='>=3.5',
       description=('The web application components of the docassemble system.'),
       long_description=read("README.md"),
