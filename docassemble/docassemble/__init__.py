18e87f7cddc2361c08db377f5aa3581338e99d64
Jonathan Pyle <jhpyle@gmail.com>
2020-12-01 07:18:41 -0500

    Bump version: 1.2.5 → 1.2.6

--- docassemble/docassemble/__init__.py
+++ docassemble/docassemble/__init__.py
@@ -2,4 +2,4 @@ try:
     __import__('pkg_resources').declare_namespace(__name__)
 except ImportError:
     __path__ = __import__('pkgutil').extend_path(__path__, __name__)
-__version__ = "1.2.5"
+__version__ = "1.2.6"
