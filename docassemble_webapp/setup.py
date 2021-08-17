ed3aefeaf8ddfc674b9b632ceb0d971af85507aa
Jonathan Pyle <jhpyle@gmail.com>
2021-08-17 07:56:43 -0400

    Bump version: 1.2.97 → 1.2.98

--- docassemble_webapp/setup.py
+++ docassemble_webapp/setup.py
@@ -6,9 +6,9 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 install_requires = [
-    'docassemble==1.2.97',
-    'docassemble.base==1.2.97',
-    'docassemble.demo==1.2.97',
+    'docassemble==1.2.98',
+    'docassemble.base==1.2.98',
+    'docassemble.demo==1.2.98',
     "3to2==1.1.1",
     "airtable-python-wrapper==0.15.2",
     "alembic==1.6.2",
@@ -245,7 +245,7 @@ install_requires = [
 ]
 
 setup(name='docassemble.webapp',
-      version='1.2.97',
+      version='1.2.98',
       python_requires='>=3.8',
       description=('The web application components of the docassemble system.'),
       long_description=read("README.md"),
