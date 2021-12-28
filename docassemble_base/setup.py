25b100c6c25e48da06312f93f6b196e0a23726dd
Jonathan Pyle <jhpyle@gmail.com>
2021-12-27 23:08:46 -0500

    Bump version: 1.3.10 → 1.3.11

--- docassemble_base/setup.py
+++ docassemble_base/setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 install_requires = [
-    'docassemble==1.3.10',
+    'docassemble==1.3.11',
     "3to2==1.1.1",
     "alembic==1.6.2",
     "astunparse==1.6.3",
@@ -189,7 +189,7 @@ install_requires = [
 ]
 
 setup(name='docassemble.base',
-      version='1.3.10',
+      version='1.3.11',
       python_requires='>=3.8',
       description=('The base components of the docassemble system.'),
       long_description=read("README.md"),
