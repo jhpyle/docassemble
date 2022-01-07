55728576990f8b4589aedcb0f909e52f3f8f0c1e
Jonathan Pyle <jhpyle@gmail.com>
2022-01-07 11:29:59 -0500

    nltk data download

--- docassemble_webapp/docassemble/webapp/server.py
+++ docassemble_webapp/docassemble/webapp/server.py
@@ -56,10 +56,19 @@ import nltk
 try:
     if not os.path.isdir(os.path.join(nltk.data.path[0], 'corpora', 'omw-1.4')):
         nltk.download('omw-1.4')
+except:
+    pass
+try:
     if not os.path.isdir(os.path.join(nltk.data.path[0], 'corpora', 'wordnet')):
         nltk.download('wordnet')
+except:
+    pass
+try:
     if not os.path.isdir(os.path.join(nltk.data.path[0], 'corpora', 'wordnet_ic')):
         nltk.download('wordnet_ic')
+except:
+    pass
+try:
     if not os.path.isdir(os.path.join(nltk.data.path[0], 'corpora', 'sentiwordnet')):
         nltk.download('sentiwordnet')
 except:
