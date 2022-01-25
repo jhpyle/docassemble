a31e3ebd03c9233a186859fb1b67878bd149fa9b
Jonathan Pyle <jhpyle@gmail.com>
2022-01-25 07:41:01 -0500

    Bump version: 1.3.19 → 1.3.20

--- docassemble_webapp/setup.py
+++ docassemble_webapp/setup.py
@@ -8,9 +8,9 @@ setup_requires = [
     'enum34==1.1.8'
     ]
 install_requires = [
-    'docassemble==1.3.19',
-    'docassemble.base==1.3.19',
-    'docassemble.demo==1.3.19',
+    'docassemble==1.3.20',
+    'docassemble.base==1.3.20',
+    'docassemble.demo==1.3.20',
     "3to2==1.1.1",
     "airtable-python-wrapper==0.15.2",
     "alembic==1.6.2",
@@ -251,7 +251,7 @@ install_requires = [
 ]
 
 setup(name='docassemble.webapp',
-      version='1.3.19',
+      version='1.3.20',
       python_requires='>=3.8',
       description=('The web application components of the docassemble system.'),
       long_description=read("README.md"),
