a14886699820bf5c83220eb7a77f423610d1ba65
Jonathan Pyle <jhpyle@gmail.com>
2023-04-14 21:56:03 -0400

    Bump version: 1.4.44 → 1.4.45

--- docassemble_base/setup.py
+++ docassemble_base/setup.py
@@ -47,7 +47,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 install_requires = [
-    'docassemble==1.4.44',
+    'docassemble==1.4.45',
     "3to2==1.1.1",
     "alembic==1.9.2",
     "astunparse==1.6.3",
@@ -233,7 +233,7 @@ else:
     install_requires.append("docassemble-backports==1.0")
 
 setup(name='docassemble.base',
-      version='1.4.44',
+      version='1.4.45',
       python_requires='>=3.8',
       description=('The base components of the docassemble system.'),
       long_description=read("README.md"),
