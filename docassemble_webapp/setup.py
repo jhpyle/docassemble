83bb05f91c2a0286ab502dc693cd0802d24e9bbb
Jonathan Pyle <jhpyle@gmail.com>
2024-02-04 22:27:50 -0500

    Bump version: 1.4.94 → 1.4.95

--- docassemble_webapp/setup.py
+++ docassemble_webapp/setup.py
@@ -10,9 +10,9 @@ setup_requires = [
     'enum34==1.1.10'
     ]
 install_requires = [
-    'docassemble==1.4.94',
-    'docassemble.base==1.4.94',
-    'docassemble.demo==1.4.94',
+    'docassemble==1.4.95',
+    'docassemble.base==1.4.95',
+    'docassemble.demo==1.4.95',
     "3to2==1.1.1",
     "acme==2.8.0",
     "aiohttp==3.9.1",
@@ -297,7 +297,7 @@ install_requires = [
 ]
 
 setup(name='docassemble.webapp',
-      version='1.4.94',
+      version='1.4.95',
       python_requires='>=3.9',
       description=('The web application components of the docassemble system.'),
       long_description=read("README.md"),
