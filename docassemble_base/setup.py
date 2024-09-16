b615ac8259efae1356db1f95ce699460e613e3a5
Jonathan Pyle <jhpyle@gmail.com>
2024-09-16 08:12:47 -0400

    Bump version: 1.5.7 → 1.5.8

--- docassemble_base/setup.py
+++ docassemble_base/setup.py
@@ -47,7 +47,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 install_requires = [
-    'docassemble==1.5.7',
+    'docassemble==1.5.8',
     "3to2==1.1.1",
     "aiohappyeyeballs==2.3.5",
     "aiohttp==3.10.3",
@@ -243,7 +243,7 @@ install_requires = [
 ]
 
 setup(name='docassemble.base',
-      version='1.5.7',
+      version='1.5.8',
       python_requires='>=3.9',
       description=('The base components of the docassemble system.'),
       long_description=read("README.md"),
